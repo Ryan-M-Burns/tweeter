ca2cc71757ba7e3dfdc4c98155a560420ae543a0
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-12 18:32:17 +0000

    implement working character counter. Numbers turn red when negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  const textArea = $("#tweet-text");
+  const $counter = $(".counter");
+  textArea.on("input", () => {
+    let count = 140 - textArea.val().length;
+    if (count < 0) {
+      $counter.css({ color: 'red' });
+    } else {
+      $counter.css({ color: "rgb(84, 81, 73)"})
+    }
+    $counter.text(count);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
