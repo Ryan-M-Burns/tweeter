8353a403d7dba014b4a87163a3398d4e69cfc626
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-14 00:20:38 +0000

    implement scrolling tweets and textarea reset. Refactor client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,6 +14,7 @@ $(document).ready(() => {
   const createTweetElement = function(tweet) {
     let difference = (Date.now() - tweet.created_at);
     let timeAgo = timeago.format((Date.now() - difference));
+    
     let $tweet = ` 
     <article class="tweet">
         <header>
@@ -37,34 +38,31 @@ $(document).ready(() => {
     return $tweet;
   };
 
+  $form.on("reset", (event) => $("#tweet-text").val("").trigger("input"));
+
 
   $form.on("submit", (event) => {
     event.preventDefault();
-
+    const dataArray = $form.serializeArray();
     const dataToSend = $form.serialize();
+    const dataToText = dataArray[0].value;
 
-    $.ajax({
-      method: "POST",
-      url: "/tweets",
-      data: dataToSend,
-      success: function(result) {
-        loadTweets();
-      }
-    });
+    if (!dataToText) {
+      return alert("No message!");
+    }
+
+    if (dataToText.length > 140) {
+      return alert("Too long!!");
+    }
+
+    $form.trigger('reset');
+
+    $.post('/tweets', dataToSend, () => loadTweets());
 
   });
 
 
-  const loadTweets = () => {
-    $.ajax({
-      method: "GET",
-      url: "/tweets",
-      success: function(result) {
-        renderTweets(result);
-      }
-    });
-  };
-
+  const loadTweets = () => $.get('/tweets', renderTweets);
 
   loadTweets();
 
