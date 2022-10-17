24803bc997473b4bf57212784f90edd96c4e7389
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-17 03:45:38 +0000

    finish creating helper functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,39 +5,37 @@ $(() => {
   const $form = $(".new-tweet");
 
   $form.on("reset", () => $("#tweet-text").val("").trigger("input"));
+  
+  $form.on("submit", generateTweets);
 
+  $(".write-tweet-button").on("click", () => toggleWriteTweet());
 
-  $(".write-tweet-button").on("click", () => {
-    $(".new-tweet").toggle(400);
-    $("textarea").focus();
-  });
+  $(window).on('scroll', () => toggleToTopButton());
 
-  $(window).on('scroll', () => { toggleToTopButton(); });
+  $(".back-to-top").on("click", () => $(window).scrollTop(0));
 
-  $(".back-to-top").on("click", () => { $(window).scrollTop(0); });
-
-  // Prepend new tweet to the existing tweets if legal tweet
-  $form.on("submit", (event) => {
-    
-    event.preventDefault();
-
-    const dataArray = $form.serializeArray();
-    const dataToSend = $form.serialize();
-    const dataToText = dataArray[0].value;
-
-    if (!isIllegalTweet(dataToText)) {
-      $form.trigger('reset');
-
-      $.post('/tweets', dataToSend, () => loadTweets());
-    }
-  });
-
-  // hides error message when tweet text box selected
   $("#tweet-text").focus(() => $("#error-box").slideUp());
 
 });
 
+
 // Helper Functions
+
+// Prepend new tweet to the existing tweets if legal tweet
+const generateTweets = (event) => {
+  const $form = $(".new-tweet");
+  event.preventDefault();
+
+  const dataArray = $form.serializeArray();
+  const dataToSend = $form.serialize();
+  const dataToText = dataArray[0].value;
+
+  if (!isIllegalTweet(dataToText)) {
+    $form.trigger('reset');
+
+    $.post('/tweets', dataToSend, () => loadTweets());
+  }
+};
 
 // Verify tweet is not empty and at or below 140 characters
 const isIllegalTweet = (tweet) => {
@@ -56,6 +54,7 @@ const isIllegalTweet = (tweet) => {
   return false;
 };
 
+
 // toggle show/hide status of return to top button
 const toggleToTopButton = () => {
   const $toTop = $(".back-to-top");
@@ -68,9 +67,17 @@ const toggleToTopButton = () => {
 };
 
 
+const toggleWriteTweet = () => {
+  $(".new-tweet").toggle(400);
+  $("textarea").focus();
+};
+
+
 const renderTweets = (tweets) => {
   const $tweetArea = $(".tweet-cage");
+
   $tweetArea.empty();
+
   $('html').css("background-image", randomBackground());
 
   for (let tweet of tweets) {
@@ -88,6 +95,7 @@ const randomBackground = () => {
   return `url("../images/backgrounds/background${randomInt}.png")`;
 
 };
+
 
 const createTweetElement = (tweet) => {
   let difference = (Date.now() - tweet.created_at);
@@ -119,6 +127,7 @@ const createTweetElement = (tweet) => {
   };
 
 };
+
 
 const loadTweets = () => $.get('/tweets', renderTweets);
 
